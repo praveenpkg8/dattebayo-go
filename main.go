f7711c5e05a6568a5aec411923ea739480343315
praveenpkg8 <praveepkg8@gmail.com>
2022-11-22 22:09:35 +0530

    Added: Main go file

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/praveenpkg8/dattebayo-go/database"
+	"github.com/praveenpkg8/dattebayo-go/server"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	database.Init()
+	server.Init()
+}
