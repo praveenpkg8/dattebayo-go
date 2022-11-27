8dd9c46f47f15b7ea7936897379757e6c666bc03
praveenpkg8 <praveepkg8@gmail.com>
2022-11-27 13:11:39 +0530

    Modified: create brothers controller

--- controllers/brothersController.go
+++ controllers/brothersController.go
@@ -1,13 +1,15 @@
 package controllers
 
 import (
-	"github.com/google/uuid"
 	"github.com/praveenpkg8/dattebayo-go/models"
 )
 
-func CreateBrothers(newBrothers models.Brothers, brotherPersonalDetails models.PersonalDetails) {
-	brotherId := uuid.New().String()
-	models.AddBrothers(brotherId, newBrothers)
-	models.AddPersonalDetails(brotherId, brotherPersonalDetails)
+func CreateBrothers(
+	newBrothers models.Brothers,
+	brotherPersonalDetails models.PersonalDetails,
+) (string, string) {
+	brotherId := models.AddBrothers(newBrothers)
+	brPerDetId := models.AddPersonalDetails(brotherId, brotherPersonalDetails)
+	return brotherId, brPerDetId
 
 }
