a5dc848735a6e37a236882b6f6663bfdd88f3f8b
praveenpkg8 <praveepkg8@gmail.com>
2023-10-23 17:35:23 +0530

    Added: personal detail controller

--- controllers/personalDetailsController.go
+++ controllers/personalDetailsController.go
@@ -2,6 +2,26 @@ package controllers
 
 import "github.com/praveenpkg8/dattebayo-go/models"
 
+func GetPerIdByBrotherId(brotherId string) (string, error) {
+	perId, err := models.GetPerIdByBrotherId(brotherId)
+	if err != nil {
+		return "", err
+	}
+	return perId, nil
+}
+
+func GetPersonalDetByBrotherID(brotherId string) (models.PersonalDetails, error) {
+	return models.GetPersonalDetByBrotherID(brotherId)
+}
+
 func UpdatePersonalContact(perDetId string, brPersonalDetail models.PersonalDetails) {
 	models.UpdateContactDetails(perDetId, brPersonalDetail)
+}
+
+func UpdateFileType(perDetId string, fileSeries string, fileExtension string, contentType string) {
+	models.UpdateFileType(perDetId, fileSeries, fileExtension, contentType)
+}
+
+func GetFileExtAndContentType(brotherId string, fileSeries string) (string, string, error) {
+	return models.GetFileExtAndContentType(brotherId, fileSeries)
 }
