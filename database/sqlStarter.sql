9ada67a5e48b9ef92e4383c39f263d6027fcfd2c
praveenpkg8 <praveepkg8@gmail.com>
2023-10-23 17:39:21 +0530

    Modified: sql starter script

--- database/sqlStarter.sql
+++ database/sqlStarter.sql
@@ -1,22 +1,28 @@
 CREATE TABLE brothers (
-	id VARCHAR(255) primary key,
-	firstName VARCHAR(255),
-	lastName VARCHAR(255),
-	sudoName VARCHAR(255),
-	referralCode VARCHAR(255),
-	referredBy VARCHAR(255), 
-	approvalStatus VARCHAR(255)
+    id VARCHAR(255) PRIMARY KEY,
+    firstName VARCHAR(255) DEFAULT '',
+    lastName VARCHAR(255) DEFAULT '',
+    sudoName VARCHAR(255) DEFAULT '',
+    referralCode VARCHAR(255) DEFAULT '',
+    referredBy VARCHAR(255) DEFAULT '',
+    emailVerificationStatus VARCHAR(255) DEFAULT '',
+    approvalStatus VARCHAR(255) DEFAULT '',
+    approvedBy VARCHAR(255) DEFAULT ''
 );
 
 CREATE TABLE personalDetails (
-	id VARCHAR(255),
-	brotherId VARCHAR(255),
-	dob VARCHAR(255),
-	email VARCHAR(255),
-	phoneNumber VARCHAR(255),
-	altPhoneNumber VARCHAR(255), 
-	altContactName VARCHAR(255), 
-	altContactRelationship VARCHAR(255), 
-	currentAddress VARCHAR(8080), 
-	permanentAddress VARCHAR(8080)
+    id VARCHAR(255) PRIMARY KEY,
+    brotherId VARCHAR(255) DEFAULT '',
+    dob VARCHAR(255) DEFAULT '',
+    email VARCHAR(255) DEFAULT '',
+    phoneNumber VARCHAR(255) DEFAULT '',
+    altPhoneNumber VARCHAR(255) DEFAULT '',
+    altContactName VARCHAR(255) DEFAULT '',
+    altContactRelationship VARCHAR(255) DEFAULT '',
+    currentAddress VARCHAR(8080) DEFAULT '',
+    currentFileType VARCHAR(255) DEFAULT '',
+    currentContentType VARCHAR(255) DEFAULT '',
+    permanentAddress VARCHAR(8080) DEFAULT '',
+    permanentFileType VARCHAR(255) DEFAULT '',
+    permanentContentType VARCHAR(255) DEFAULT ''
 );
