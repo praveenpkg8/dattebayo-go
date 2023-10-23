5463a10a70d7b527f1801addb3a836fc7dcd12c6
praveenpkg8 <praveepkg8@gmail.com>
2023-10-23 17:39:44 +0530

    Modified: brothers go

--- models/brothers.go
+++ models/brothers.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/google/uuid"
@@ -10,10 +12,10 @@ import (
 )
 
 type Address struct {
+	PhoneNo    string `json:"phone_no"`
 	DoorNo     string `json:"door_no"`
 	StreetName string `json:"street_name"`
 	Area       string `json:"area"`
-	District   string `json:"district"`
 	State      string `json:"state"`
 	Country    string `json:"country"`
 	PinCode    string `json:"pin_code"`
@@ -29,28 +31,40 @@ type PersonalDetails struct {
 	AltContactName         string  `json:"alt_contact_name"`
 	AltContactRelationship string  `json:"alt_contact_relationship"`
 	CurrentAddress         Address `json:"current_address"`
+	CurrentFileType	string `json:"current_file_type"`
+	CurrentContentType	string `json:"current_content_type"`
 	PermanentAddress       Address `json:"permanent_address"`
+	PermanentFileType	string `json:"permanent_file_type"`
+	PermanentContentType	string `json:"permanent_content_type"`
 }
 
 type Brothers struct {
-	Id             string `json:"id,omitempty"`
-	FirstName      string `json:"first_name"`
-	LastName       string `json:"last_name"`
-	SudoName       string `json:"sudo_name"`
-	ReferralCode   string `json:"referral_code"`
-	ReferredBy     string `json:"referred_by"`
-	ApprovalStatus string `json:"approval_status"`
+	Id                      string `json:"id,omitempty"`
+	FirstName               string `json:"first_name"`
+	LastName                string `json:"last_name"`
+	SudoName                string `json:"sudo_name"`
+	ReferralCode            string `json:"referral_code"`
+	ReferredBy              string `json:"referred_by"`
+	EmailVerificationStatus string `json:"email_verification_status"`
+	ApprovalStatus          string `json:"approval_status"`
+	ApprovedBy              string `json:"approvedBy"`
 }
 
-func AddBrothers(newBrothers Brothers) string {
+type BrotherResponse struct {
+	Id               string `json:"id"`
+	PersonalDetailId string `json:"personal_detail_id"`
+}
+
+func AddBrothers(newBrothers Brothers) (string, error) {
 	log.Println("Adding brother")
-	var err error
 	var db *sql.DB = database.GetDB()
 	brotherId := uuid.New().String()
-	stmt, err := db.Prepare("INSERT INTO brothers(id, firstName, lastName, referralCode) values(?, ?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO brothers(id, firstName, lastName, sudoName, referralCode) VALUES(?, ?, ?, ?)")
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+	defer stmt.Close()
+
 	_, err = stmt.Exec(
 		brotherId,
 		newBrothers.FirstName,
@@ -58,12 +72,11 @@ func AddBrothers(newBrothers Brothers) string {
 		newBrothers.ReferralCode,
 	)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	defer stmt.Close()
-	log.Printf("Added Brother: %v | %v | %v \n", brotherId, newBrothers.FirstName, newBrothers.LastName)
-	return brotherId
+	log.Printf("Added Brother: %v | %v | %v\n", brotherId, newBrothers.FirstName, newBrothers.LastName)
+	return brotherId, nil
 }
 
 func AddPersonalDetails(brotherId string, brotherPersonalDetails PersonalDetails) string {
@@ -88,4 +101,88 @@ func AddPersonalDetails(brotherId string, brotherPersonalDetails PersonalDetails
 	defer stmt.Close()
 	log.Printf("Added PersonalDetails : %v | %v | %v \n", id.String(), brotherId, brotherPersonalDetails.Email)
 	return id.String()
+}
+
+func VerifyBrother(brotherId string, approvalStatus string, approvedBy string) {
+	log.Printf("Adding update brothers contact")
+	var err error
+	var db *sql.DB = database.GetDB()
+
+	stmt, err := db.Prepare(
+		"UPDATE brothers SET " +
+			"approvalStatus = ?, approvedBy = ?" +
+			"WHERE id = ?",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(
+		approvalStatus,
+		approvedBy,
+		brotherId,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+}
+
+func GetBrotherById(brotherId string) (Brothers, error) {
+	var db *sql.DB = database.GetDB()
+	query := "SELECT * FROM brothers WHERE id = ?"
+	rows, err := db.Query(query, brotherId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var brothers Brothers // Create a Brothers struct to store the retrieved data
+		if err := rows.Scan(&brothers.Id, &brothers.FirstName, &brothers.LastName, &brothers.SudoName, &brothers.ReferralCode, &brothers.ReferredBy, &brothers.EmailVerificationStatus, &brothers.ApprovalStatus, &brothers.ApprovedBy); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("ID: %s, First Name: %s, Last Name: %s, ...\n", brothers.Id, brothers.FirstName, brothers.LastName)
+		return brothers, nil
+	} else {
+		return Brothers{}, errors.New("no results found") // Return an empty Brothers struct
+	}
+}
+
+func GetAllBrothers() ([]Brothers, error) {
+	var db *sql.DB = database.GetDB()
+	query := "SELECT * FROM brothers"
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var brothersList []Brothers
+
+	for rows.Next() {
+		var brothers Brothers
+		if err := rows.Scan(
+			&brothers.Id,
+			&brothers.FirstName,
+			&brothers.LastName,
+			&brothers.SudoName, // Scan as NullString.String
+			&brothers.ReferralCode,
+			&brothers.ReferredBy,
+			&brothers.EmailVerificationStatus,
+			&brothers.ApprovalStatus,
+			&brothers.ApprovedBy,
+		); err != nil {
+			log.Fatal(err)
+		}
+		brothersList = append(brothersList, brothers)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return brothersList, nil
 }
